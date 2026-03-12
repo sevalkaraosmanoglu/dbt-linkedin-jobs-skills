70a78b6a7b8860f7f60e07168a72c121e0459d2e
Seval Karaosmanoğlu <sevallacin@gmail.com>
2026-03-12 10:56:43 +0000

    adding java count for skill software analysis

--- models/marts/fact_job_skills_summary.sql
+++ models/marts/fact_job_skills_summary.sql
@@ -4,7 +4,8 @@ with job_skills as (
     select
         job_id_int,
         max(if(strpos(skill, 'SQL') > 0, 1, 0)) as has_sql,
-        max(if(strpos(skill, 'Python') > 0, 1, 0)) as has_python
+        max(if(strpos(skill, 'Python') > 0, 1, 0)) as has_python,
+        max(if(strpos(skill, 'Java') > 0, 1, 0)) as has_java
     from {{ ref('fact_job_skills') }}
     group by job_id_int
 )
